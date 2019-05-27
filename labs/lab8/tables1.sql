609282bfa50fb23811b6de86d6ad4ca8350cdd88
jackryan135 <jackryan135@gmail.com>
2019-05-27 16:25:21 -0700

    end of lab 8

---
+++ labs/lab8/tables1.sql
@@ -0,0 +1,10 @@
+DROP TABLE MovieReviews;
+
+Create table MovieReviews (movieName varchar(25), comments varchar(40));
+
+Create or Replace procedure addMovieReview (v_movieName IN varchar, v_comment IN varchar) AS 
+BEGIN
+        insert into MovieReviews values (v_movieName,v_comment);
+END;
+/
+show errors;
