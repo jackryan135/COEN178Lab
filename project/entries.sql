43228ae5540cd91ca251b88289b9bea774ec1223
jackryan135 <jackryan135@gmail.com>
2019-06-04 16:11:46 -0700

    fixed entry format

--- project/entries.sql
+++ project/entries.sql
@@ -1,1 +1,1 @@
-INSERT INTO Customers VALUES ("0001", "Jack", "jack@scu.edu", "1050 Benton St", "05-JUN-19", "regular");
+INSERT INTO Customers VALUES ('0001', 'Jack', 'jack@scu.edu', '1050 Benton St', '05-JUN-19', 'gold');
