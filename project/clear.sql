666b51602370e5be18d5de577cc6d1b3da5e790c
Jack Ryan <jackryan135@gmail.com>
2019-06-04 16:06:30 -0700

    entry check

---
+++ project/clear.sql
@@ -0,0 +1,5 @@
+DROP TABLE Customers;
+DROP TABLE Orders;
+DROP TABLE TShirt;
+DROP TABLE ComicBooks;
+DROP TABLE StoreItems;
---
+++ project/entries.sql
@@ -0,0 +1,1 @@
+INSERT INTO Customers VALUES ("0001", "Jack", "jack@scu.edu", "1050 Benton St", "2019-05-24", "regular");
