74cab80f4afb33873471d60d105265300431de84
Jack Ryan <jackryan135@gmail.com>
2019-06-04 17:38:02 -0700

    fix

--- project/procedures.sql
+++ project/procedures.sql
@@ -13,7 +13,7 @@ BEGIN
 
 	IF(l_membership = 'regular') THEN
 		l_fee := 10.00;
-	ELSEIF(l_membership = 'gold')
+	ELSEIF(l_membership = 'gold') THEN
 		l_fee := 0.00;
 	ELSE
 		RAISE invalidMembership;
